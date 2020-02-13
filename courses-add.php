ba385d6dc948bcf84a012daf0743116e53eff81d
Awais <nisarawais246@gmail.com>
2020-02-13 12:13:27 -0500

    trying to fix on saving the data to the database

--- courses-add.php
+++ courses-add.php
@@ -1,31 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Add New Course</title>
+    <h1>Add New Course</h1>
 </head>
 <body>
-<?php
-
-$code = null;
-$name = null;
-$teacher = null;
-$time = null;
-// connect
-
-$db = new PDO('mysql:host=172.31.22.43;dbname=Awais1110642', 'Awais1110642', 'flA8Dz-xDy');
-
-$courses = $cmd->fetch();
-$code = $courses['code'];
-$name= $courses['name'];
-$teacher = $courses['teacher'];
-$time = $courses['time'];
-
-// disconnect
-$db = null;
-}
-?>
 <!--this code below this line will directed to the another site where the user will be informed that the data is been stored into the database-->
 <form action = "courses-saved.php" method="post">
     <fieldset>
