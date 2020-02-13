9444bd21566b78aad1cfaacefbe28c69568134d2
Awais <nisarawais246@gmail.com>
2020-02-13 16:10:20 -0500

    fixed the issues with the table

--- course-list.php
+++ course-list.php
@@ -13,7 +13,7 @@ $cmd->execute();
 $courses = $cmd->fetchAll();
 echo "<table border='1'><thead><th>Course Code</th><th>Course Name</th><th>Teacher</th><th>Time</th></thead>";
 foreach ($courses as $data) {
-    echo "<tr><td>" . $data['code'] . "</td><td>" . $data['name'] . "</td><td>" . $data['teacher'] .$data['time'] . "</td><td>";
+    echo "<tr><td>" . $data['code'] . "</td><td>" . $data['name'] . "</td><td>" . $data['teacher']."</td><td>" .$data['time'] . "</td><td>";
 }
 echo "</table>";
 $db = null;
--- student-list.php
+++ student-list.php
@@ -13,7 +13,7 @@ $cmd->execute();
 $students = $cmd->fetchAll();
 echo "<table border='1'><thead><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Gender</th><th>Grade</th></thead>";
 foreach ($students as $data) {
-    echo "<tr><td>" . $data['student_id'] . "</td><td>" . $data['first_name'] . "</td><td>" . $data['last_name'] .$data['gender'] . "</td><td>" . $data['grade'] . "</td></tr>";
+    echo "<tr><td>" . $data['student_id'] . "</td><td>" . $data['first_name'] . "</td><td>" . $data['last_name']. "</td><td>".$data['gender'] . "</td><td>" . $data['grade'] . "</td></tr>";
 }
 echo "</table>";
 $db = null;
