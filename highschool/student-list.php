00ddd36fe8ad004625e30bc11ef96b06d374534f
aanisar <55286102+aanisar@users.noreply.github.com>
2020-03-15 21:03:51 -0400

    Add files via upload
    Fixing the parts where it need to be fix by:
    * Adding the bin parameter

---
+++ highschool/student-list.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student List</title>
+</head>
+<body>
+<?php
+//connected
+$db = new PDO('mysql:host=172.31.22.43;dbname=Awais1110642', 'Awais1110642', 'flA8Dz-xDy');
+$query = "select * from students;";
+$cmd = $db->prepare($query);
+$cmd->execute();
+$students = $cmd->fetchAll();
+// creating a design for the table
+echo "<table border='1'><thead><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Gender</th><th>Grade</th></thead>";
+foreach ($students as $data) {
+    echo "<tr><td>" . $data['student_id'] . "</td><td>" . $data['first_name'] . "</td><td>" . $data['last_name']. "</td><td>".$data['gender'] . "</td><td>" . $data['grade'] . "</td></tr>";
+}
+echo "</table>";
+/// disconnected
+$db = null;
+?>
+</body>
+</html>
