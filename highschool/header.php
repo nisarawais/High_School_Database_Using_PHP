d8ffae5e5ed5ca8b163e366104b6adef5091f26b
aanisar <55286102+aanisar@users.noreply.github.com>
2020-03-26 11:23:28 -0400

    Add files via upload
    I added the home page and explans what the purposes of this site. I also added the header for the title.

---
+++ highschool/header.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--This echo title in php will be inidviudal title for the specific page-->
+    <title>Awais High School for the Deaf ~ <?php echo $title; ?></title>
+</head>
+<body>
+<header>
+<h1>Awais High School for the Deaf</h1>
+<h2><?php echo $title;?></h2>
+</header>
+    
+</body>
+</html>
